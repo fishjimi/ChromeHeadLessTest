23eeb0dd425bb5fb5b9b0aa676f586b6fadbfbb0
jimifish <269911478@qq.com>
2018-03-18 06:07:02 +0800

    添加项目文件。

---
+++ ChromeHeadLessTest/Program.cs
@@ -0,0 +1,121 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace ChromeHeadLessTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments(new List<string>() { "headless"});
+            //chromeOptions.EnableMobileEmulation("iPhone X");
+            ChromeDriver driver = new ChromeDriver(Directory.GetCurrentDirectory(), chromeOptions);
+            driver.Manage().Window.Size = new Size(1920, 1080);
+            driver.Navigate().GoToUrl("https://www.bilibili.com/");
+            var wait = new WebDriverWait(driver, new TimeSpan(10));
+            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.ClassName("footer")));
+            Console.WriteLine("打开了网页");
+            var screenshot = driver.GetScreenshot();
+            screenshot.SaveAsFile("a.png");
+            Console.WriteLine("截了一张图");
+            var entereScreenshot = GetEntireScreenshot(driver);
+            entereScreenshot.Save("b.png");
+            Console.WriteLine("截了全图");
+            Console.ReadKey();
+            driver.Close();
+        }
+
+        /// <summary>
+        /// 截全屏方法
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <returns></returns>
+        public static Image GetEntireScreenshot(ChromeDriver driver)
+        {
+            // Get the total size of the page
+            var totalWidth = (int)(long)((IJavaScriptExecutor)driver).ExecuteScript("return document.body.offsetWidth"); //documentElement.scrollWidth");
+            var totalHeight = (int)(long)((IJavaScriptExecutor)driver).ExecuteScript("return  document.body.parentNode.scrollHeight");
+            // Get the size of the viewport
+            var viewportWidth = (int)(long)((IJavaScriptExecutor)driver).ExecuteScript("return document.body.clientWidth"); //documentElement.scrollWidth");
+            var viewportHeight = (int)(long)((IJavaScriptExecutor)driver).ExecuteScript("return window.innerHeight"); //documentElement.scrollWidth");
+
+            // We only care about taking multiple images together if it doesn't already fit
+            if (totalWidth <= viewportWidth && totalHeight <= viewportHeight)
+            {
+                var screenshot = driver.GetScreenshot();
+                return ScreenshotToImage(screenshot);
+            }
+            // Split the screen in multiple Rectangles
+            var rectangles = new List<Rectangle>();
+            // Loop until the totalHeight is reached
+            for (var y = 0; y < totalHeight; y += viewportHeight)
+            {
+                var newHeight = viewportHeight;
+                // Fix if the height of the element is too big
+                if (y + viewportHeight > totalHeight)
+                {
+                    newHeight = totalHeight - y;
+                }
+                // Loop until the totalWidth is reached
+                for (var x = 0; x < totalWidth; x += viewportWidth)
+                {
+                    var newWidth = viewportWidth;
+                    // Fix if the Width of the Element is too big
+                    if (x + viewportWidth > totalWidth)
+                    {
+                        newWidth = totalWidth - x;
+                    }
+                    // Create and add the Rectangle
+                    var currRect = new Rectangle(x, y, newWidth, newHeight);
+                    rectangles.Add(currRect);
+                }
+            }
+            // Build the Image
+            var stitchedImage = new Bitmap(totalWidth, totalHeight);
+            // Get all Screenshots and stitch them together
+            var previous = Rectangle.Empty;
+            foreach (var rectangle in rectangles)
+            {
+                // Calculate the scrolling (if needed)
+                if (previous != Rectangle.Empty)
+                {
+                    var xDiff = rectangle.Right - previous.Right;
+                    var yDiff = rectangle.Bottom - previous.Bottom;
+                    // Scroll
+                    ((IJavaScriptExecutor)driver).ExecuteScript(String.Format("window.scrollBy({0}, {1})", xDiff, yDiff));
+                }
+                // Take Screenshot
+                var screenshot = driver.GetScreenshot();
+                // Build an Image out of the Screenshot
+                var screenshotImage = ScreenshotToImage(screenshot);
+                // Calculate the source Rectangle
+                var sourceRectangle = new Rectangle(viewportWidth - rectangle.Width, viewportHeight - rectangle.Height, rectangle.Width, rectangle.Height);
+                // Copy the Image
+                using (var graphics = Graphics.FromImage(stitchedImage))
+                {
+                    graphics.DrawImage(screenshotImage, rectangle, sourceRectangle, GraphicsUnit.Pixel);
+                }
+                // Set the Previous Rectangle
+                previous = rectangle;
+            }
+            return stitchedImage;
+        }
+
+        private static Image ScreenshotToImage(Screenshot screenshot)
+        {
+            Image screenshotImage;
+            using (var memStream = new MemoryStream(screenshot.AsByteArray))
+            {
+                screenshotImage = Image.FromStream(memStream);
+            }
+            return screenshotImage;
+        }
+    }
+}
